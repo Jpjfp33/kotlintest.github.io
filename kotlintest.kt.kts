640b3759589fab9e317e51a7e2731823ae7836e5
Jpjfp33 <168223173+Jpjfp33@users.noreply.github.com>
2024-08-22 23:09:39 +0100

    Add files via upload

---
+++ kotlintest.kt.kts
@@ -0,0 +1,52 @@
+fun main() {
+
+printIn("Dados das Línguas de Programação escolhidas na escola")
+// Dados das Línguas de Programação escolhidas na escola
+
+}
+
+val estudantes = 10
+println("There are $estudantes who chose kotlin")
+// There are 10 estudantes who chose kotlin
+
+println("There are ${estudantes + 5} estudantes que who chose kotlin")
+// There are 15 estudantes who chose kotlin
+
+var estudantes = 15
+
+// Some estudantes leave the queue
+estudantes = 8
+
+estudantes = estudantes + 3 // Example of addition: 11
+customers += 7            // Example of addition: 18
+customers -= 3            // Example of subtraction: 15
+customers *= 2            // Example of multiplication: 30
+customers /= 3            // Example of division: 10
+
+val year: Int = 2020
+
+val score: UInt = 100u
+
+val currentTemp: Float = 24.5f, val price: Double = 19.99
+
+val isEnabled: Boolean = true
+
+val separator: Char = ','
+
+val message: String = "Hello, world!"
+
+// Variable declared without initialization
+val d: Int
+// Variable initialized
+d = 3
+
+// Variable explicitly typed and initialized
+val e: String = "hello"
+
+// Variables can be read because they have been initialized
+println(d) // 3
+println(e) // hello
+
+
+
+
